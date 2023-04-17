849a2208f383b38019efc2dfdac10eaedca57805
unknown <adarshrg1998@gmail.com>
2023-04-17 17:31:19 +0530

    first commit

---
+++ src/app/products/view-all-products/view-all-products.component.ts
@@ -0,0 +1,21 @@
+import { Component,OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-view-all-products',
+  templateUrl: './view-all-products.component.html',
+  styleUrls: ['./view-all-products.component.css']
+})
+export class ViewAllProductsComponent implements OnInit {
+  productData:any
+  filterCategory:any
+  constructor(private ps:ProductService){}
+  ngOnInit():void{
+    this.ps.viewAllProduct().subscribe((data:any)=>{
+      this.productData=data;
+    })
+  }
+filter(category:any){
+  this.filterCategory=this.productData.filter((i:any)=>i.categoryId==category||category=="")
+  }
+}
